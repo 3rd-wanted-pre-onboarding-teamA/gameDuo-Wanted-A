6a4deea41cf0e9883012588138f44649addad9ca
dltmddus1998 <dltmddus1998@naver.com>
2022-07-13 18:42:39 +0900

    Feat: bossRaid 시작및종료 API 기능개발

--- controller/bossRaid.controller.js
+++ controller/bossRaid.controller.js
@@ -1,99 +1,137 @@
-const { default: axios } = require('axios');
+const { default: axios } = require("axios");
 const redis = require("redis");
 const BossRaidService = require("../services/bossRaid.service.js");
 const redisClient = redis.createClient();
 
 class BossRaidController {
-    // 보스레이드 게임 시작
-    static async startBossRaid(req, res) {
-        const { userId, level } = req.body;
-        let isEntered;
+  // 보스레이드 게임 시작
+  static async startBossRaid(req, res) {
+    const { userId, level } = req.body;
+    const [data] = await BossRaidService.createId(userId, level); // raidRecordId 생성
+    const raidRecordId = data.insertId;
+    let isEntered = false;
 
-        const [data] = await BossRaidService.createId(userId, level);
-        const raidRecordId = data.insertId;
-        // console.log(data.insertId);
-        // 레이드 시작 가능 -> 중복되지 않는 raidRecordId 생성 / isEntered=true
-        // if (레이드 시작 가능) { // !enteredUserId && canEnter===false
-            isEntered=true;
-        // } else { // encounteredUserId
-            // isEntered=false;
-        // }
-
-        return res.status(201).json({
-            message: "BossRaid Start!!",
-            isEntered,
-            raidRecordId,
-        })
+    /**
+     * Redis에서 raidStatus가 있으면 이미 사용중이므로 게임 시작이 불가능하고
+     * 반대의 경우 게임 시작이 가능하다
+     */
+    try {
+      await redisClient.connect();
+      const value = await redisClient.get("raidStatus");
+      if (value) {
+        // 게임 시작 불가능
+        isEntered = true;
+        return res.status(400).json({
+          message: "이미 게임중인 사용자가 있습니다.",
+          isEntered,
+        });
+      } else {
+        // 게임 시작 가능
+        await redisClient.set("raidStatus", raidRecordId);
+        isEntered = true;
+      }
+    } catch (err) {
+      throw err;
+    } finally {
+      await redisClient.disconnect();
     }
 
-    // 보스레이드 게임 종료
-    static async stopBossRaid(req, res) {
-        let singleScore; // 게임 종료 후 총점에 합산할 점수
-        let bossRaidData;
-        const { userId, raidRecordId } = req.body;
-        const [totalScore] = await BossRaidService.findTotalScore(userId);
-        let { score } = totalScore[0];
-        const [data] =  await BossRaidService.findLevel(raidRecordId);
-        const { user_id, boss_raid_level } = data[0];
+    return res.status(201).json({
+      message: "BossRaid Start!!",
+      isEntered,
+      raidRecordId,
+    });
+  }
 
-        /**
-         * 게임 레벨 별 점수 관련 static data
-         * 💽 Redis에 캐싱하여 사용하기
-         */
+  // 보스레이드 게임 종료
+  static async stopBossRaid(req, res) {
+    let singleScore; // 게임 종료 후 총점에 합산할 점수
+    let bossRaidData;
+    const { userId, raidRecordId } = req.body;
+    const [totalScore] = await BossRaidService.findTotalScore(userId);
+    let { score } = totalScore[0];
+    const [data] = await BossRaidService.findLevel(raidRecordId);
+    const { user_id, boss_raid_level, enter_time } = data[0];
 
-        await redisClient.connect(); // Redis Client 연결
-        const value = await redisClient.get("bossRaidData"); 
-        if (value) {
-            console.log("from cached data");
-            bossRaidData = JSON.parse(value);
-        } else {
-            const {data} = await axios("https://dmpilf5svl7rv.cloudfront.net/assignment/backend/bossRaidData.json");
-            const dataJson = JSON.stringify(data);
-            await redisClient.set("bossRaidData", dataJson);
-            console.log("from source data");
-            bossRaidData = dataJson;
-        }
-        const staticData = bossRaidData.bossRaids[0];
-        const { bossRaidLimitSeconds, levels } = staticData;
+    /**
+     * 게임 레벨 별 점수 관련 static data
+     * 💽 Redis에 캐싱하여 사용하기
+     */
+    try {
+      await redisClient.connect(async (conn) => conn);
+      const value = await redisClient.get("bossRaidData");
+      if (value) {
+        console.log("from cached data");
+      } else {
+        const { data } = await axios("https://dmpilf5svl7rv.cloudfront.net/assignment/backend/bossRaidData.json");
+        const dataJson = JSON.stringify(data);
+        await redisClient.set("bossRaidData", dataJson);
+        console.log("from source data");
+      }
+      bossRaidData = JSON.parse(value);
+      const staticData = bossRaidData.bossRaids[0];
+      const { bossRaidLimitSeconds, levels } = staticData;
 
-        /** 📍 유효성 검사 - 예외 처리
-         * 1. 저장된 userId !== raidRecordId (userId와 raidRecordId를 이용하여 boss_raid table에서 찾아낸 userId가 다른 경우!)
-         * 2. 레이드 제한시간 out
-         */
-        // 1. 
-        if (user_id !== userId) {
-            return res.status(403).json({
-                message: "아이디가 다르므로 접근 불가합니다."
-            });
-        }
-        
-        // console.log(bossRaidLimitSeconds); 180
-        // if ()
-        
-
-        /* 
-        * 방금 게임을 끝낸 raidRecordId가 진행한 게임 레벨을 찾아서
-        * 해당 레벨의 점수를 해당 user_id의 score에 합산하자.
-        */
-        levels.forEach(info => {
-            if (boss_raid_level === info.level) {
-                singleScore = info.score;
-                score = score + singleScore;
-            } 
+      /** 📍 유효성 검사 - 예외 처리
+       * 1. 저장된 userId !== raidRecordId (userId와 raidRecordId를 이용하여 boss_raid table에서 찾아낸 userId가 다른 경우!)
+       * 2. 레이드 제한시간 out
+       */
+      // 1.
+      if (user_id !== userId) {
+        return res.status(403).json({
+          message: "아이디가 다르므로 접근 불가합니다.",
         });
+      }
+      // 2.
+      let enter_time2 = enter_time.split(" ");
+      let end_time = new Date();
+      let enter_time3 = new Date(enter_time2[0] + " " + enter_time2[1]);
 
-        res.status(200).json({
-            message: "게임종료",
-            bossRaidEndData: {
-                userId,
-                raidRecordId,
-                boss_raid_level,
-                totalScore: score,
-                singleScore,
-            }
+      const date = new Date(+new Date() + 3240 * 10000).toISOString().split("T")[0];
+      const time = new Date().toTimeString().split(" ")[0];
+      const end_time_date = `${date} ${time}`;
+
+      // console.log(date + ' ' + time);
+
+      if ((end_time.getTime() - enter_time3.getTime()) / 1000 > bossRaidLimitSeconds) {
+        return res.status(400).json({
+          message: "레이드 제한시간을 넘었으므로 기록에 남지 않습니다.",
         });
-        
+      }
+
+      // 게임 종료후 end_time 입력
+      await BossRaidService.putEndTime(raidRecordId, end_time_date);
+
+      // redisStatus 삭제
+      await redisClient.del("raidStatus");
+
+      /*
+       * 방금 게임을 끝낸 raidRecordId가 진행한 게임 레벨을 찾아서
+       * 해당 레벨의 점수를 해당 user_id의 score에 합산하자.
+       */
+      levels.forEach((info) => {
+        if (boss_raid_level === info.level) {
+          singleScore = info.score;
+          score = score + singleScore;
+        }
+      });
+    } catch (err) {
+      throw err;
+    } finally {
+      await redisClient.disconnect();
     }
+
+    res.status(200).json({
+      message: "게임종료",
+      bossRaidEndData: {
+        userId,
+        raidRecordId,
+        boss_raid_level,
+        totalScore: score,
+        singleScore,
+      },
+    });
+  }
 }
 
 module.exports = BossRaidController;
--- services/bossRaid.service.js
+++ services/bossRaid.service.js
@@ -1,58 +1,66 @@
 const pool = require("../db/mysqlConfig.js");
 
 class BossRaidService {
-    // 중복되지 않는 raidRecordId 생성
-    static async createId(userId, level) {
-        const sql = `INSERT INTO boss_raid (user_id, boss_raid_level) VALUES (?)`;
-        const values = [
-            [userId, level]
-        ];
-        let connection = null;
-        try {
-            connection = await pool.getConnection(async (conn) => conn);
-            return await connection.query(sql, values);
-        } catch (err) {
-            console.error(err);
-            throw err;
-        } finally {
-            connection.release();
-        }
+  // 중복되지 않는 raidRecordId 생성
+  static async createId(userId, level) {
+    const sql = `INSERT INTO boss_raid (user_id, boss_raid_level) VALUES (?)`;
+    const values = [[userId, level]];
+    let connection = null;
+    try {
+      connection = await pool.getConnection(async (conn) => conn);
+      return await connection.query(sql, values);
+    } catch (err) {
+      console.error(err);
+      throw err;
+    } finally {
+      connection.release();
     }
+  }
 
-    //
-    // static async findUserIdFromBossRaid(raidRecordId) {
-    //     const sql = `SELECT user_id FROM boss_raid WHERE raid_record_id=${raidRecordId}`;
-    // }
-
-    // 게임 끝난 후 점수 합산을 위한 boss_raid_level 찾기 (boss_raid table)
-    static async findLevel(raidRecordId) {
-        const sql = `SELECT user_id, boss_raid_level FROM boss_raid WHERE raid_record_id=${raidRecordId}`;
-        let connection = null;
-        try {
-            connection = await pool.getConnection(async (conn) => conn);
-            return await connection.query(sql);
-        } catch (err) {
-            console.error(err);
-            throw err;
-        } finally {
-            connection.release();
-        }
+  // 게임 끝난 후 점수 합산을 위한 boss_raid_level 찾기 (boss_raid table)
+  static async findLevel(raidRecordId) {
+    const sql = `SELECT user_id, boss_raid_level, enter_time FROM boss_raid WHERE raid_record_id=${raidRecordId}`;
+    let connection = null;
+    try {
+      connection = await pool.getConnection(async (conn) => conn);
+      return await connection.query(sql);
+    } catch (err) {
+      console.error(err);
+      throw err;
+    } finally {
+      connection.release();
     }
+  }
 
-    // 게임 끝난 후 점수 합산을 위한 총점 찾기 (user table)
-    static async findTotalScore(userId) {
-        const sql = `SELECT score FROM user WHERE user_id=${userId}`;
-        let connection = null;
-        try {
-            connection = await pool.getConnection(async (conn) => conn);
-            return await connection.query(sql);
-        } catch (err) {
-            console.error(err);
-            throw err;
-        } finally {
-            connection.release();
-        }
+  // 게임 끝난 후 점수 합산을 위한 총점 찾기 (user table)
+  static async findTotalScore(userId) {
+    const sql = `SELECT score FROM user WHERE user_id=${userId}`;
+    let connection = null;
+    try {
+      connection = await pool.getConnection(async (conn) => conn);
+      return await connection.query(sql);
+    } catch (err) {
+      console.error(err);
+      throw err;
+    } finally {
+      connection.release();
     }
+  }
+
+  // 게임 끝난 후 게임 종료 시간 입력
+  static async putEndTime(raidRecordId, end_time) {
+    const sql = `UPDATE boss_raid SET end_time=${end_time} WHERE raid_record_id=${raidRecordId}`;
+    let connection = null;
+    try {
+      connection = await pool.getConnection(async (conn) => conn);
+      return await connection.query(sql);
+    } catch (err) {
+      console.error(err);
+      throw err;
+    } finally {
+      connection.release();
+    }
+  }
 }
 
 module.exports = BossRaidService;
