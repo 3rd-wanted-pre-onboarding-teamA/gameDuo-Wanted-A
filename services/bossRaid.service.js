f68902748692838801a773ff52073b1c13bd9cf4
username <greenrock46@gmail.com>
2022-07-13 16:43:24 +0900

    Refactor: 보스레이드 레디스 데이터 조회 메소드 수정 * 변경 사항   * redis의 client의 연결상태 파악하는 코드 추가   * npm redis v4버전 업그레이드에 따른 문법 변경에 맞게  수정

--- services/bossRaid.service.js
+++ services/bossRaid.service.js
@@ -1,20 +1,28 @@
 const redis = require("redis");
-const mysqlPool = require("../db/mysqlConfig");
 const redisPool = require("../db/redisConfig");
 
 class BossRaidService {
   static bossRaidStatus = async function () {
-    const client = redis.createClient(redisPool);
+    const client = redis.createClient({
+      redisPool, 
+      legacyMode: true
+    });
+    client.on("connect", () => {
+      console.info("Rdedis connected!");
+    });
+    client.on("error", (err) => {
+      console.error("Redis Client Error", err)
+    });
     try {
       await client.connect();
-      const status = await client.get("raidStatus");
+      const redisCli = client.v4;
+      const status = await redisCli.get("raidStatus");
       return parseInt(status, 10);
     } catch (err) {
       throw err;
     } finally {
       await client.disconnect();
     }
-
   };
 }
 
