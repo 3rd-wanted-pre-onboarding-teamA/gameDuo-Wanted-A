64db77fe5f12177b984adccf65886aee2d1bbbd7
hjy <heojungyeon.97@gmail.com>
2022-07-13 09:29:14 +0900

    Refactor: DDL 수정

--- db/gameduo_boss_raid_pve.sql
+++ db/gameduo_boss_raid_pve.sql
@@ -1,18 +1,18 @@
 create database gameduo_boss_raid_pve;
 use gameduo_boss_raid_pve;
 create table user ( -- 유저
-    user_id int not null,
-    score int,
-    primary key(user_id)
+  user_id varchar(50) not null,
+  score int,
+  primary key(user_id)
 );
-create table boss_raid ( -- 가계부
-    raid_record_id int not null auto_increment,
-    user_id int not null,
-    enter_time datetime not null, 
-    end_time datetime not null, 
-    boss_raid_level int not null,
-    success boolean not null,
-    primary key(raid_record_id),
-    constraint boss_raid_user_fk FOREIGN KEY (user_id)
-    REFERENCES user(user_id) ON UPDATE CASCADE
+create table boss_raid ( -- 보스레이드
+  raid_record_id int not null auto_increment,
+  user_id varchar(50) not null,
+  enter_time datetime default now(), 
+  end_time datetime, 
+  boss_raid_level int not null,
+  success boolean default false,
+  primary key(raid_record_id),
+  constraint boss_raid_user_fk FOREIGN KEY (user_id)
+  REFERENCES user(user_id) ON UPDATE CASCADE
 );
