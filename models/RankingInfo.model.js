2fca437d062a784dddc82f610cce250281197308
hjy <heojungyeon.97@gmail.com>
2022-07-14 12:18:49 +0900

    Delete: 파일 삭제

--- models/RankingInfo.model.js
+++
@@ -1,8 +0,0 @@
-let RankingInfo = class {
-  constructor(ranking, userId, totalScore) {
-    this.ranking = ranking;
-    this.userId = userId;
-    this.totalScore = totalScore;
-  }
-}
-module.exports = RankingInfo;
