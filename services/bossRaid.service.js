ca97be9d4c2e26883e3131fb20a92a2cc43a9527
dltmddus1998 <dltmddus1998@naver.com>
2022-07-14 13:51:46 +0900

    Feat: 보스레이드 게임 시작 및 종료 기능 개발3

--- services/bossRaid.service.js
+++ services/bossRaid.service.js
@@ -17,6 +17,149 @@ class BossRaidService {
     }
   };
 
+  // 중복되지 않는 raidRecordId 생성
+  static async createId(userId, level) {
+    const sql = `INSERT INTO boss_raid (user_id, boss_raid_level) VALUES (?)`;
+    const values = [[userId, level]];
+    let connection = null;
+    try {
+      connection = await mysqlPool.getConnection(async (conn) => conn);
+      return await connection.query(sql, values);
+    } catch (err) {
+      console.error(err);
+      throw err;
+    } finally {
+      connection.release();
+    }
+  }
+
+  // raidStatus에 따른 게임 시작 여부 
+  static async bossRaidStatus() {
+    const client = redis.createClient(redisPool);
+    try {
+      await client.connect();
+      const status = await client.get("raidStatus");
+      return parseInt(status, 10);
+    } catch (err) {
+      throw err;
+    } finally {
+      await client.disconnect();
+    }
+  }
+
+  // 게임 시작 가능하면 raidStatus에 raidRecordId 넣기
+  static async putRaidRecordId(raidRecordId) {
+    const client = redis.createClient(redisPool);
+    try {
+        await client.connect();
+        await client.set("raidStatus", raidRecordId);
+    } catch {
+        throw err;
+    } finally {
+        await client.disconnect();
+    }
+  }
+
+  static async levelCahceToRedis() {
+    // 게임 레벨 별 점수 관련 static data 캐싱 (Redis 사용)
+    const client = redis.createClient(redisPool);
+    try {
+        await client.connect();
+        let staticData = await client.get("bossRaidData");
+        return staticData;
+    } catch (err) {
+        throw err;
+    } finally {
+        await client.disconnect();
+    }
+  }
+
+  static async putStaticData(data) {
+    // redis 캐시에 점수관련 static data 없으면 캐싱하기
+    const client = redis.createClient(redisPool);
+    try {
+        await client.connect();
+        await client.set("bossRaidData", data);
+    } catch (err) {
+        throw err;
+    } finally {
+        await client.disconnect();
+    }
+  }
+
+  static async delRedisStatus() {
+    // redisStatus 삭제 (게임 종료 후)
+    const client = redis.createClient(redisPool);
+    try {
+        await client.connect();
+        await client.del("raidStatus");
+    } catch (err) {
+        throw err;
+    } finally {
+        await client.disconnect();
+    }
+  }
+
+  // 게임 끝난 후 점수 합산을 위한 boss_raid_level 찾기 (boss_raid table)
+  static async findLevel(raidRecordId) {
+    const sql = `SELECT user_id, boss_raid_level, enter_time FROM boss_raid WHERE raid_record_id=${raidRecordId}`;
+    let connection = null;
+    try {
+      connection = await mysqlPool.getConnection(async (conn) => conn);
+      return await connection.query(sql);
+    } catch (err) {
+      console.error(err);
+      throw err;
+    } finally {
+      connection.release();
+    }
+  }
+
+  // 게임 끝난 후 점수 합산을 위한 총점 찾기 (user table)
+  static async findTotalScore(userId) {
+    const sql = `SELECT score FROM user WHERE user_id=${userId}`;
+    let connection = null;
+    try {
+      connection = await mysqlPool.getConnection(async (conn) => conn);
+      return await connection.query(sql);
+    } catch (err) {
+      console.error(err);
+      throw err;
+    } finally {
+      connection.release();
+    }
+  }
+
+  // 게임 끝난 후 게임 종료 시간 입력
+  static async putEndTime(raidRecordId, end_time) {
+    const sql = `UPDATE boss_raid SET end_time="${end_time}" WHERE raid_record_id=${raidRecordId}`;
+    let connection = null;
+    try {
+      connection = await mysqlPool.getConnection(async (conn) => conn);
+      return await connection.query(sql);
+    } catch (err) {
+      console.error(err);
+      throw err;
+    } finally {
+      connection.release();
+    }
+  }
+
+  // fail 저장
+  static async setSuccess(raidRecordId) {
+    const sql = `UPDATE boss_raid SET success=true WHERE raid_record_id=${raidRecordId}`;
+    let connection = null;
+    try {
+      connection = await mysqlPool.getConnection(async (conn) => conn);
+      return await connection.query(sql);
+    } catch (err) {
+      console.error(err);
+      throw err;
+    } finally {
+      connection.release();
+    }
+  }
+
   static async topRankerInfoList() {
     // Top 10 랭커 redis에서 추출
     let client = null;
